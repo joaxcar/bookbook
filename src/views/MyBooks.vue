f65c10dff64df2edd13e90ca831171586879e11a
data-pata <tsvangirai@outlook.com>
2020-01-09 12:24:16 +0100

    begin addbooks to collection in firestore

--- src/views/MyBooks.vue
+++ src/views/MyBooks.vue
@@ -2,6 +2,19 @@
   <h1>My books View</h1>
 </template>
 
-<script></script>
+<script>
+
+import firebase from 'firebase';
+const db = firebase.firestore();
+
+export default {
+  mounted() {
+    db.collections.('mybooks').get().then((snapshot) => {
+      console.log(snapshot.docs);
+    });
+  }
+
+};
+</script>
 
 <style scoped></style>
