a26764eaf061d64df9ae67da7eaba24564e9b559
data-pata <tsvangirai@outlook.com>
2020-01-09 15:57:57 +0100

    Trying to get documents from firestore but get error: index.cjs.js?e89a:351 Uncaught (in promise) FirebaseError: Missing or insufficient permissions. from line 15 at MyBooks.vue. tried: - set export GOOGLE_APPLICATION_CREDENTIALS='[PATH].json' to service account api - changed security rules to read, write: if true - error recieved both from localhost and deployed

--- src/views/MyBooks.vue
+++ src/views/MyBooks.vue
@@ -1,20 +1,41 @@
 <template>
-  <h1>My books View</h1>
+  <div class="mybooks-container">
+    <h1>My books View</h1>
+    <button @click.prevent="getDocs">test getDocs()</button>
+  </div>
 </template>
 
 <script>
-
-import firebase from 'firebase';
-const db = firebase.firestore();
+import db from "@/main";
 
 export default {
-  mounted() {
-    db.collections.('mybooks').get().then((snapshot) => {
-      console.log(snapshot.docs);
-    });
+  mounted() {},
+  methods: {
+    getDocs() {
+      window.console.log("test logging");
+      db.collection("mybooks")
+        .get()
+        .then(snapshot => {
+          window.console.log(snapshot.docs);
+        });
+    }
   }
-
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+button {
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-shadow: inset 0 1px 3px #ddd;
+  border-radius: 4px;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
+</style>
