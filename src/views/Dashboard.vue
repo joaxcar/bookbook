6fa5b146a230af060ef13d22fefb4a557608482f
jokrau <johnkristoferaugustsson@gmail.com>
2020-01-13 09:46:53 +0100

    working ean scanner

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -13,6 +13,7 @@
           type="text"
           @keydown.enter="getBook"
         ></v-text-field>
+        <v-btn>&#128247;</v-btn>
       </v-col>
     </v-row>
     <v-row>
@@ -55,12 +56,18 @@
         </v-card>
       </v-col>
     </v-row>
+    <v-btn @click="start">engage video search va</v-btn>
+    <div id="scanner-wrapper">
+      <div id="interactive" class="viewport" />
+    </div>
   </v-container>
 </template>
 
 <script>
 import getBook from "@/data/GoogleAPI";
 import Debounce from "@/utils/debounce";
+import Quagga from "quagga";
+// import { is } from "@babel/types";
 
 export default {
   data: function() {
@@ -68,9 +75,57 @@ export default {
       message: "",
       books: [],
       searchText: "9781405924412",
-      show: false
+      show: false,
+      readerSize: {
+        width: 640,
+        height: 480
+      },
+      quaggaState: {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#interactive"),
+          constraints: {
+            width: 640,
+            height: 480,
+            facingMode: ""
+          }
+        },
+        decoder: {
+          readers: ["ean_reader"],
+          debug: {
+            drawBoundingBox: true
+          }
+        },
+        locate: true,
+        locator: {
+          halfSample: true,
+          patchSize: "small",
+          debug: {
+            showCanvas: true,
+            showPatches: true,
+            showFoundPatches: true
+          }
+        }
+      }
     };
   },
+  // mounted() {
+  //   this.$nextTick(() => {
+  //     Quagga.init(this.quaggaState, function(err) {
+  //       if (err) {
+  //         window.console.log(err);
+  //         return;
+  //       }
+  //       window.console.log("init complete");
+  //       Quagga.start();
+  //       Quagga.onDetected(result => {
+  //         let isbn = result.codeResult.code;
+  //         window.console.log(isbn);
+  //       });
+  //     });
+  //   });
+  // },
   created() {
     this.debounceGetBooks = this.debounce(
       function() {
@@ -82,11 +137,65 @@ export default {
   methods: {
     get: getBook,
     debounce: Debounce,
+    start() {
+      Quagga.init(this.quaggaState, function(err) {
+        if (err) {
+          window.console.log(err);
+          return;
+        }
+        window.console.log("init complete");
+        Quagga.start();
+        Quagga.onDetected(result => {
+          let isbn = result.codeResult.code;
+          window.console.log(isbn);
+        });
+      });
+    },
     getBook() {
       getBook(this.searchText).then(ret => {
         ret.items.map(book => (this.books = [book, ...this.books]));
         this.searchText = "";
       });
+    },
+    getVideoStreamQuagga() {
+      Quagga.init({
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#scanner-area"),
+          constraints: {
+            width: 480,
+            height: 320,
+            facingMode: "user"
+          }
+        },
+        decoder: {
+          readers: ["ean_reader", "code_128_reader"]
+        },
+        debug: {
+          showCanvas: true
+        }
+      });
+    },
+    async getVideoStream() {
+      // prompt user: is ok to use your cam dude?
+      // to require rear cam, use:
+      // video: {
+      //   facingMode: {
+      //     exact: "environment"
+      //   }
+      // }
+      let videoStream = null;
+
+      try {
+        videoStream = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: "user"
+        });
+      } catch (error) {
+        window.console.log(error);
+      }
+      window.console.log(videoStream);
     }
   }
 };
