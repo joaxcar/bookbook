c5be8f589afa946d2dd3cce69a669cb1d10029d4
Johan Carlsson <johan.axel.carlsson@gmail.com>
2020-01-14 11:26:56 +0100

    barcode scanner is now a seperate component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -19,16 +19,8 @@
         ></v-text-field>
       </v-col>
     </v-row>
-    <v-row>
-      <div id="scanner-wrapper" v-show="this.isScanning">
-        <div id="interactive" class="viewport">
-          <v-overlay absolute>
-            <div class="overlay">
-              <v-icon align="center" size="70">mdi-barcode-scan</v-icon>
-            </div>
-          </v-overlay>
-        </div>
-      </div>
+    <v-row v-show="isScanning">
+      <barcode-scanner :is-scanning="isScanning" @search="searchCamera" />
     </v-row>
     <v-row align="center" justify="center">
       <v-col v-if="books.length === 0 && !isScanning" align="center">
@@ -87,13 +79,16 @@
 </template>
 
 <script>
+import BarcodeScanner from "@/components/BarcodeScanner";
 import getBooks from "@/data/GoogleAPI";
 import Debounce from "@/utils/debounce";
 import { mapState } from "vuex";
-import Quagga from "quagga";
 import fs from "@/data/fs";
 
 export default {
+  components: {
+    "barcode-scanner": BarcodeScanner
+  },
   computed: {
     ...mapState(["data"])
   },
@@ -110,30 +105,6 @@ export default {
       readerSize: {
         width: innerWidth,
         height: innerHeight
-      },
-      quaggaState: {
-        inputStream: {
-          name: "Live",
-          type: "LiveStream",
-          target: document.querySelector("#interactive"),
-          constraints: {
-            width: innerWidth,
-            height: innerHeight,
-            facingMode: "environment"
-          }
-        },
-        decoder: {
-          readers: ["ean_reader"],
-          debug: {
-            drawBoundingBox: true,
-            drawScanLine: true
-          }
-        },
-        locate: true,
-        locator: {
-          halfSample: true,
-          patchSize: "large"
-        }
       }
     };
   },
@@ -158,37 +129,7 @@ export default {
   methods: {
     debounce: Debounce,
     toggleScan() {
-      this.isScanning ? this.stopScan() : this.startScan();
-    },
-    startScan() {
-      this.isScanning = true;
-      let self = this;
-      Quagga.init(this.quaggaState, function(err) {
-        if (err) {
-          window.console.log(err);
-          return;
-        }
-        window.console.log("init complete");
-        Quagga.start();
-        Quagga.onDetected(
-          function(result) {
-            if (result) {
-              window.console.log(result.codeResult.code);
-              window.console.log(self);
-              let isbn = result.codeResult.code;
-              if (isbn.startsWith("978")) {
-                self.searchText = "isbn:" + isbn;
-                self.getBook();
-                self.stopScan();
-              }
-            }
-          }.bind(this)
-        );
-      });
-    },
-    stopScan() {
-      this.isScanning = false;
-      Quagga.stop();
+      this.isScanning = !this.isScanning;
     },
     searchForBooks() {
       if (this.isScanning) this.stopScan();
@@ -215,6 +156,13 @@ export default {
         ];
       });
     },
+    searchCamera(isbn) {
+      this.searchText = "isbn:" + isbn;
+      getBooks(this.searchText, 0).then(ret => {
+        this.$router.push("/details/" + ret.items[0].id);
+      });
+      this.lastSearch = this.searchText;
+    },
     addBook(book) {
       if (!this.data.books.some(item => item.id === book.id)) fs.addBook(book);
       //this.$store.dispatch("addBook", book);
@@ -227,24 +175,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.v-overlay {
-  position: relative;
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-  text-transform: uppercase;
-  font-weight: bold;
-}
-
-.overlay {
-  position: absolute;
-  background-color: white;
-  opacity: 0.8;
-  margin-top: 2em;
-  margin-left: -2em;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  top: 0;
-}
-</style>
